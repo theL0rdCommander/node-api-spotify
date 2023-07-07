2a4cdd9b481eb0a9759658d3ff3ccd0f2d46ac1a
Felipe Ignacio Garcia Maidana <felipe.ignacio.garcia@fing.edu.uy>
2023-07-07 00:49:08 -0300

    comentarios

--- index.js
+++ index.js
@@ -12,16 +12,18 @@ const REDIRECT_URI  =process.env.REDIRECT_URI
 const statekey = 'spotify_auth_state'
 //let code_verifier = generateRandomString(128);
 
-//#1 inicia requiriendo la authorization
+//#1 inicia requiriendo la autorization
 app.get('/auth/login', (req, res) => {
     
+    //scope contiene los distintos permisos que requerimos al usuario
     var scope = "streaming \
     user-read-email \
     user-read-private"  
     var state = generateRandomString(16);
-    
-//  codeChallenge = generateCodeChallenge(code_verifier);
-//  res.cookie('code_verifier', code_verifier);
+ 
+    //  codeChallenge = generateCodeChallenge(code_verifier);
+    //  res.cookie('code_verifier', code_verifier);
+    // proximamente se utilizarian, pero el login funciona sin ellas por ahora.
 
     res.cookie(statekey, state);
     
@@ -31,14 +33,16 @@ app.get('/auth/login', (req, res) => {
         client_id: CLIENT_ID,
         scope: scope,
         state: state,
-        //code_challenge_method: 'S256',
+        //code_challenge_method: 'S256',    //seguramente sean credenciales que se pidan a futuro
         //code_challenge: codeChallenge
     })
+
     // peticion de redireccion a la app principal 
     console.log("redirecting to spotify!")
     res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
 })
 
+//esta ruta redirecciona desde spotify, una vez el usuario acepta darnos los permisos
 app.get('/auth/callback', (req, res) => {
     const code = req.query.code || null;
   
@@ -55,9 +59,23 @@ app.get('/auth/callback', (req, res) => {
         Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
       },
     })
-      .then(response => {
+    .then(response => {
         if (response.status === 200) {
-          res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+    
+          const { access_token, token_type } = response.data;
+    
+          axios.get('https://api.spotify.com/v1/me', {
+            headers: {
+              Authorization: `${token_type} ${access_token}`
+            }
+          })
+            .then(response => {
+              res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+            })
+            .catch(error => {
+              res.send(error);
+            });
+    
         } else {
           res.send(response);
         }
@@ -67,15 +85,32 @@ app.get('/auth/callback', (req, res) => {
       });
   });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  //Utilizada cuando pasan 3600 segundos, se tiene que autenticar de nuevo, esta vez con un refresh_token
+  app.get('/refresh_token', (req, res) => {
+    const { refresh_token } = req.query;
+  
+    axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      data: querystring.stringify({
+        grant_type: 'refresh_token',    //Diferencia al estar logueado
+        refresh_token: refresh_token
+      }),
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+      },
+    })
+      .then(response => {
+        res.send(response.data);
+      })
+      .catch(error => {
+        res.send(error);
+      });
+   });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
+   
+   //Utilizada para generar string random en el intercambio de datos con la api
 var generateRandomString = function (length) {
     var text = '';
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -100,3 +135,11 @@ async function generateCodeChallenge(codeVerifier) {
 
     return base64encode(digest);
 }  
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
